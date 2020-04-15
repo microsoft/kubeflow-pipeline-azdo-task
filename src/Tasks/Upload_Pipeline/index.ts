24bd3685eab143897e28baa317ccd04774bb9422
Ryan Zuber <v-ryzube@microsoft.com>
2020-04-15 15:43:47 -0700

    Initialized gitignore, file structure, and json files

---
+++ src/Tasks/Upload_Pipeline/index.ts
@@ -0,0 +1,4 @@
+import path = require("path");
+import fs = require("fs");
+import task = require("azure-pipelines-task-lib/task");
+import { IExecSyncResult } from "azure-pipelines-task-lib/toolrunner";
